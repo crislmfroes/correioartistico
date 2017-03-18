2763a59a7add00d7ca6d95bff51506546ba1a313
crislmfroes <cris.lima.froes@gmail.com>
2017-03-18 18:23:05 -0300

    Cadastra cartas.

--- sorteia.php
+++ sorteia.php
@@ -2,11 +2,23 @@
 $connect = mysqli_connect('mysql.hostinger.com.br', 'u343668054_kmkz', 'c11bb875ddef1f7');
 mysqli_select_db('u343668054_kmkz', $connect);
 $db_estudantes = mysqli_query($connect, "SELECT matricula, turma FROM estudantes");
-$db_cartas = mysqli_query($connect, "SELECT numero, estudantes, data_entrega FROM cartas");
-if ($db_cartas && $db_estudantes) {
-	$db_cartas = mysqli_fetch_all($db_cartas);
+if ($db_estudantes) {
 	$db_estudantes = mysqli_fetch_all($db_estudantes);
 } else {
+	die();
+}
+if(!mysqli_query($connect, "SELECT 1 FROM cartas RESTRICT 1")){
+	foreach ($db_estudantes as $estudante) {
+		$numero = $estudante["carta"];
+		$matricula = serialize(array($estudante["matricula"]));
+		mysqli_query($connect, "INSERT INTO cartas(numero, estudantes) VALUES ('$numero', '$matricula')");
+	}
+}
+$db_cartas = mysqli_query($connect, "SELECT numero, estudantes, id FROM cartas");
+if($db_cartas){
+	$db_cartas = mysqli_fetch_all($db_cartas);
+}
+else{
 	die();
 }
 $matriculas_2c = array();
